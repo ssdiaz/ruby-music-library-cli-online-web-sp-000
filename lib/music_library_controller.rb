57b46d2847ad59ec1bd859ce51c92f0db8c64a79
Sam D <samidiaz3@gmail.com>
2021-04-06 16:28:54 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -71,7 +71,7 @@ class MusicLibraryController
 
     if genre = Genre.find_by_name(input)
       genre.songs.sort {|a,b| a.name <=> b.name}.each_with_index{ |song, index|
-        puts "#{index+1}. #{song.artist} - #{song.name}"}
+        puts "#{index+1}. #{song.artist.name} - #{song.name}"}
       end
   end
 
