77239204a780b8266b9d6e1e0098b03f77a2ec44
Sam D <samidiaz3@gmail.com>
2021-04-06 14:45:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,7 @@
 class MusicLibraryController
 
   attr_accessor :path
-  attr_reader :artist, :name
+  attr_reader :artist, :name, :path
 
   def initialize(path= "./db/mp3s")
     MusicImporter.new(path).import
