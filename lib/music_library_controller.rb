7d2d3438f843cf22a5762daa51ccafccb4a874cb
Sam D <samidiaz3@gmail.com>
2021-04-06 14:57:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -58,12 +58,12 @@ class MusicLibraryController
   #  input = ""
     puts "Please enter the name of an artist:"
     input = gets.chomp
-    # Song.all.find{|input| input == artist}.each_with_index{ |song, index|
-    #   puts "#{index+1}. #{song}"
-    # }
-    Song.all.each_with_index{ |song, index|
-      puts "#{index+1}. #{song.name}"
+     Song.all.find{|input| input == artist#}.each_with_index{ |song, index|
+      puts "#{index+1}. #{song}"
     }
+    # Song.all.each_with_index{ |song, index|
+    #   puts "#{index+1}. #{song.name}"
+    # }
 
   end
 
