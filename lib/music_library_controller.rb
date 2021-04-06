9bbeda579a91fdb12a514b997d68679468539eb4
Sam D <samidiaz3@gmail.com>
2021-04-06 17:28:08 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -58,7 +58,7 @@ require 'pry'
 
   def list_songs_by_artist
     puts "Please enter the name of an artist:"
-    input = gets.chomp
+    input = gets.strip
 
     if artist = Artist.find_by_name(input)
       artist.songs.sort {|a,b| a.name <=> b.name}.each_with_index{|song, index|
