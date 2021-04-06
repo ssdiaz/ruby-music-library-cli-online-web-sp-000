7d99e287b6b1611c36a02f16ed083fd95e55b50c
Sam D <samidiaz3@gmail.com>
2021-04-06 14:37:28 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,7 +41,7 @@ class MusicLibraryController
 
   def list_artists
     Artist.all.sort{ |a,b| a.name <=> b.name}.each_with_index{ |artist, index|
-      puts "#{name}"
+      puts "#{index+1}. #{name}"
     }
   end
 
