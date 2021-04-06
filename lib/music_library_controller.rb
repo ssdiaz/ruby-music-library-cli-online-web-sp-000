e94ba7d6d8b44bd3ccac99ac004dcc60c756d3fd
Sam D <samidiaz3@gmail.com>
2021-04-06 18:49:21 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -82,9 +82,11 @@ require 'pry'
     input = gets.strip.to_i #this is a number int
 
     if (1 ... Song.all.length).include?(input)
+        Song.all[input-1]
        puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
+  
 end
 # rspec spec/010_music_library_controller_spec.rb
 # rspec spec/011_music_library_cli_methods_spec.rb
