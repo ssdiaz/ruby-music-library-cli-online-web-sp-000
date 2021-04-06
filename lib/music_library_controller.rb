41ea3ed0cc657ba5cd76abdf1dac8c1726a7c511
Sam D <samidiaz3@gmail.com>
2021-04-06 16:25:05 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -70,7 +70,7 @@ class MusicLibraryController
     input = gets.chomp
 
     if genre = Genre.find_by_name(input)
-      genre.songs.sort {|a,b| a.name <=> b.name}.each_with_index{ |genre, index|
+      genre.songs.sort {|a,b| a.name <=> b.name}.each_with_index{ |song, index|
         puts "#{index+1}. #{song.name} - #{song.genre.name}"}
       end
   end
