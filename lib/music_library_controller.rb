f2aa505ab08942a47ac9f293b453024e10c7e4fc
Sam D <samidiaz3@gmail.com>
2021-04-06 14:20:43 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -34,7 +34,9 @@ class MusicLibraryController
   end
 
   def list_songs
-    print Song.all.each_with_index.sort {|song, index|
+
+    array = Song.all.sort
+    print array.each_with_index {|song, index|
       puts "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     }
   end
