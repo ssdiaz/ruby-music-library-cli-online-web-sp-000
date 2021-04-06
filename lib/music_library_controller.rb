3680b8f97871e4d647e82faca951c0d98af03f99
Sam D <samidiaz3@gmail.com>
2021-04-06 14:57:02 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -62,7 +62,7 @@ class MusicLibraryController
     #   puts "#{index+1}. #{song}"
     # }
     Song.all.each_with_index{ |song, index|
-      puts "#{index+1}. #{song}"
+      puts "#{index+1}. #{song.name}"
     }
 
   end
