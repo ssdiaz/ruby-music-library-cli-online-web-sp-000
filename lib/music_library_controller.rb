b5ba17bf2f3bb64268aef04eb905b2fa6f6d64bc
Sam D <samidiaz3@gmail.com>
2021-04-06 14:28:59 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -38,7 +38,7 @@ class MusicLibraryController
     # array = Song.all.sort do |a,b| a<=> b end
     # array.each_with_index {|song, index|
     # Song.all.each_with_index.sort {|song, index|
-    Song.all.sort.each_with_index {|song, index|
+    Song.all.sort {|a,b| a<=>b}.each_with_index {|song, index|
       print "#{index+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
     }
   end
